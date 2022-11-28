ba74bba2633e9cdf36c0ad5633a1c82be0d70d57
Eiker <eiker.gonzalez21>
2022-11-28 10:32:54 -0400

    ajustes smn_despacho_consumos

--- WEB-INF/action/smn_registro/smn_despacho/procesar/actualizar-despacho_EN.sql
+++ WEB-INF/action/smn_registro/smn_despacho/procesar/actualizar-despacho_EN.sql
@@ -2,7 +2,8 @@ UPDATE smn_inventario.smn_despacho SET
 	des_estatus = 'EN',
 	smn_zona_rf=${fld:smn_zona_rf},
 	smn_ruta_id=${fld:smn_ruta_id},
-	smn_transporte_id=${fld:smn_transporte_id}
+	smn_transporte_id=${fld:smn_transporte_id},
+	des_fecha_despacho={d '${def:date}'}
 WHERE
 	smn_despacho_id = ${fld:smn_despacho_id}
 RETURNING smn_despacho_id as despachoId;
